6aab84dc8d28e8a538df0d155df54c948efbf968
ryuto0712 <oginoryuto@icloud.com>
2021-09-24 10:27:11 +0900

    動画表示は完了

---
+++ lib/view/screens/intro_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class IntroScreen extends StatefulWidget {
+
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  final video = 'assets/IMG_4065.mp4';
+VideoPlayerController? _controller;
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = VideoPlayerController.asset(video)..initialize().then((_){
+      _controller!.play();
+      _controller!.setLooping(true);
+      setState(() {
+        
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: 
+      VideoPlayer(_controller!)
+    );
+  }
+}
