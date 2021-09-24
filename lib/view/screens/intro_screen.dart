8290fe32cb9975566f71f3702cf5fb3b834a59df
ryuto0712 <oginoryuto@icloud.com>
2021-09-24 10:36:40 +0900

    動画のファイル形式を変えてみる

--- lib/view/screens/intro_screen.dart
+++ lib/view/screens/intro_screen.dart
@@ -2,33 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class IntroScreen extends StatefulWidget {
-
   @override
   _IntroScreenState createState() => _IntroScreenState();
 }
 
 class _IntroScreenState extends State<IntroScreen> {
   final video = 'assets/IMG_4065.mp4';
-VideoPlayerController? _controller;
+  VideoPlayerController? _controller;
 
-@override
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _controller = VideoPlayerController.asset(video)..initialize().then((_){
-      _controller!.play();
-      _controller!.setLooping(true);
-      setState(() {
-        
+    _controller = VideoPlayerController.asset(video)
+      ..initialize().then((_) {
+        _controller!.play();
+        _controller!.setLooping(true);
+        setState(() {});
       });
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: 
-      VideoPlayer(_controller!)
-    );
+    return Scaffold(body: SizedBox(child: VideoPlayer(_controller!)));
   }
 }
