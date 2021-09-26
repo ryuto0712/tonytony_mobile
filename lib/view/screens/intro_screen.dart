2dd22cb0dd300d30897e86ccd51565d4a4c45539
ryuto0712 <oginoryuto@icloud.com>
2021-09-26 23:26:09 +0900

    タイトルの表示

--- lib/view/screens/intro_screen.dart
+++ lib/view/screens/intro_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tonytony_mobile/view/widgets/login_widget.dart';
 import 'package:video_player/video_player.dart';
 
 class IntroScreen extends StatefulWidget {
@@ -24,7 +25,18 @@ class _IntroScreenState extends State<IntroScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SizedBox(child: VideoPlayer(_controller!)));
+    return Scaffold(
+        body: Stack(children: [
+      SizedBox.expand(
+        child: FittedBox(
+            fit: BoxFit.cover,
+            child: SizedBox(
+                //nullチェックしてる
+                width: _controller?.value.size.width ?? 0,
+                height: _controller?.value.size.height ?? 0,
+                child: VideoPlayer(_controller!))),
+      ),
+      LoginWidget(),
+    ]));
   }
 }
-//aa
